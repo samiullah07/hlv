0ba2c25aa06b675f093ee7a3810d2d4dfb5745dd
Anas Jahangir <100097449+samiullah07@users.noreply.github.com>
2024-04-19 23:09:15 +0500

    done

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 
-import Hero from './Hero/Hero'
-import Feature from './Feature/feature'
-import PopularBurger from './Users/popularUsers'
-import Newsletter from './Newsletter/Newsletter'
-import Experience from './Experience/Experience'
+import Hero from "./Hero/Hero";
+import Feature from "./Feature/feature";
+import PopularBurger from "./Users/popularUsers";
+import Newsletter from "./Newsletter/Newsletter";
+import Experience from "./Experience/Experience";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <Feature/>
-      <PopularBurger/>
-      <Experience/>
-      <Newsletter/>
+      <Hero />
+      <Feature />
+      <PopularBurger />
+      <Experience />
+      <Newsletter />
     </div>
-  )
-  
-}
+  );
+};
 
 export default Home;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
-import Image from 'next/image'
-import React from 'react'
-import Home from './components/Home'
-import Feature from './components/Feature/feature'
+import Image from "next/image";
+import React from "react";
+import Home from "./components/Home";
+import Feature from "./components/Feature/feature";
 
 const HomePage = () => {
   return (
     <div className=" overflow-hidden">
-      <Home/>
-      
-    
+      <Home />
     </div>
-    
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
